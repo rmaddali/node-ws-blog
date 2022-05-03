c95c02c2c5d6f335a3c82c59cee0245a1169dcc0
EC2 Default User <ec2-user@ip-172-31-39-16.us-west-2.compute.internal>
2022-05-03 16:06:52 +0000

    final commit

--- deploy/mesh/apply-virtual-gateway-ingress.sh
+++ deploy/mesh/apply-virtual-gateway-ingress.sh
@@ -7,4 +7,4 @@ echo "Image repo ${IMAGE_REPO}"
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-echo "yaml file" | sed "s#{{IMAGE_REPO}}#$IMAGE_REPO#g" ./deploy/mesh/mesh-all-new.yaml | kubectl apply -f -
+echo "yaml file" | sed "s#{{IMAGE_REPO}}#$IMAGE_REPO#g;s#{{AWS_REGION}}#$AWS_REGION#g" ./deploy/mesh/mesh-all.yaml | kubectl apply -f -
